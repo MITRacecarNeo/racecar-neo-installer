6212065c58eeddcf41900291794552ca54e90849
Chris Lai <chrisclai02@gmail.com>
2024-07-29 06:11:10 -0400

    fixed simulator update script

--- racecar-student/scripts/updatesim.sh
+++ racecar-student/scripts/updatesim.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SIM_URL="https://github.com/MITRacecarNeo/RacecarNeo-Simulator/archive/refs/tags"
+# static variables
+SIM_URL="https://github.com/MITRacecarNeo/RacecarNeo-Simulator.git"
 
 # Get the full path of the current script
 SCRIPT_PATH=$(realpath "$0")
@@ -8,16 +9,31 @@ SCRIPT_PATH=$(realpath "$0")
 # Extract the directory from the full path
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-echo 'Welcome to the RACECAR Neo command-line simulator update tool for Windows, Mac, and Linux.'
+# Extract the racecar-student directory
+RACECAR_DIR=$(dirname "$SCRIPT_DIR")
 
+echo 'Welcome to the RACECAR Neo command-line simualtor updater for Windows, Mac, and Linux.'
 
-echo '[1/2] Select your operating system: [windows, mac, linux]'
+echo '[1/1] Select your operating system: [windows, mac, linux]'
 select PLATFORM in windows mac linux
 
 do
     case $PLATFORM in
         windows|mac|linux)
-            
+            cd "$SCRIPT_DIR"/..
+            cd ..
+
+            # Remove current sim files
+            rm -rf RacecarNeo-Simulator
+
+            # Clone file from github, format dirs
+            git clone -b "${PLATFORM}" --single-branch "${SIM_URL}"
+
+            # Allow permissions
+            if [ "$PLATFORM" == 'mac' ]; then
+                chmod -R 777 RacecarNeo-Simulator
+            fi
+
             break
             ;;
         *)
@@ -25,33 +41,4 @@ do
     esac
 done
 
-echo '[2/2] Select the version you would like to download:'
-select VERSION in 2.0.0
-
-do
-    case $VERSION in
-        2.0.0)
-            break
-            ;;
-        *)
-            ;;
-    esac
-done
-
-TARGET="${SIM_URL}/${PLATFORM}_v${VERSION}.zip"
-# Go two folders back from scripts directory
-cd "$SCRIPT_DIR"/..
-cd ..
-# Remove current RacecarSim directory
-rm -rf "RacecarSim"
-# Pull file from github, unzip and place in dir, format dirs
-wget -qO- $TARGET | busybox unzip -
-SIMNAME="RacecarSim-binary-${PLATFORM}_${RELEASE_DATE}"
-mv "$SIMNAME" RacecarSim
-
-# Allow permissions
-if [ "$PLATFORM" == 'mac' ]; then
-    chmod -R 777 RacecarSim
-fi
-
-echo "RacecarSim successfully updated to version v${$VERSION} on ${PLATFORM}"
+echo 'Simulator Update Complete'
